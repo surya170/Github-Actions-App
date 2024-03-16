ddccce7cf121c6dda7cc3ad61bf6e4df433c4255
surya9014 <suryanarayana13271@gmail.com>
2024-03-16 22:58:19 +0530

    Changed JDK version

--- src/main/java/com/surya/GithubActionsCicdApp4Application.java
+++ src/main/java/com/surya/GithubActionsCicdApp4Application.java
@@ -8,15 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 public class GithubActionsCicdApp4Application {
-    /*
-    echo "# Github-Actions-App" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/surya170/Github-Actions-App.git
-git push -u origin main
-     */
+   
     @GetMapping("/welcome")
     public String welcome() {
         return "Welcome To SpringBootApp";
